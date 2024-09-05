73e42af83d9dd3721b030d3e3c9da0bc47e8a93e
uuuuow <19370603867@163.com>
2024-09-06 00:59:19 +0800

    源文件

---
+++ charserver/build/src/client/CMakeFiles/ChatClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/now/charserver/bin/ChatClient"
+  "/home/now/charserver/bin/ChatClient.pdb"
+  "CMakeFiles/ChatClient.dir/main.cpp.o"
+  "CMakeFiles/ChatClient.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
